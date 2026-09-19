616740cd4815b212b15fc25acc8accfcfc4c9da3
Bruno Dzogovic <73134171+brunodzogovic@users.noreply.github.com>
2026-09-19 20:45:42 +0200

    feat(i18n): add Norwegian Bokmål locale

--- src/utils/language.ts
+++ src/utils/language.ts
@@ -17,6 +17,7 @@ export const languageCodeMap: Record<string, string> = {
   Indonesian: 'id',
   Korean: 'ko',
   Nepali: 'np',
+  'Norwegian Bokmål': 'nb-NO',
   Persian: 'fa',
   Portuguese: 'pt',
   'Simplified Chinese': 'zh-CN',
